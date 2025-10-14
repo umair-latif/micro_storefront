86987854f27941c76e1ee6e2328d25e8cbc62cac
umair-latif <umair.latifm@gmail.com>
2025-10-14 16:57:24 +0200

    Update BrandBar.tsx

--- nextjs/app/admin/components/BrandBar.tsx
+++ nextjs/app/admin/components/BrandBar.tsx
@@ -9,6 +9,7 @@ import CopyStoreURL from "./CopyStoreURL";
 import { useDrawer } from "./DrawerProvider";
 import NewStoreButton from "@/app/admin/profile/ui/NewStoreButton";
 import { useMedia } from "@/lib/useMedia";
+import { useState } from "react";
 
 export default function BrandBar() {
 const search = useSearchParams();
